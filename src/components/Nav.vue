923cac992cee680309ecfcb9117baf07bf048d13
Shanejiang1 <jiangshanghang@gmail.com>
2020-10-28 18:04:49 +0800

    创建Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <Icon name="money"></Icon>
+      记账
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="labels"></Icon>
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"></Icon>
+      统计
+    </router-link>
   </div>
 </template>
 
