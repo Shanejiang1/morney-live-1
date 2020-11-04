30d595d951ff58ad4b92aa95362d86ccd29ba83f
Shanejiang1 <jiangshanghang@gmail.com>
2020-11-04 17:15:07 +0800

    把record复制保存至list

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -55,8 +55,10 @@ export default class NumberPad extends Vue {
   clear() {
     this.output = '0';
   }
-  ok(){
+
+  ok() {
     this.$emit('update:value', this.output);
+    this.$emit('submit', this.output);
   }
 }
 </script>
