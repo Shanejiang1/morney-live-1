55868aaf09a7e001664a51f5c5d5d2f4ebf333e3
Shanejiang1 <jiangshanghang@gmail.com>
2020-11-05 16:06:08 +0800

    将tag的 string 改为 {id,name}

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue';
+import EditLabel from '@/views/EditLabel.vue';
+
 
 Vue.use(VueRouter);
 
@@ -25,8 +27,12 @@ const routes: Array<RouteConfig> = [
     component: Statistics
   },
   {
+    path: '/labels/edit',
+    component: EditLabel
+  },
+  {
     path: '*',
-    component:NotFound
+    component: NotFound
   },
 ];
 
