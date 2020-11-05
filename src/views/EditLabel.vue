648a2e3dc40e5d90afbbdf367ab10460457bc642
Shanejiang1 <jiangshanghang@gmail.com>
2020-11-05 19:52:34 +0800

    解决 id 生成问题

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -38,18 +38,24 @@ export default class EditLabel extends Vue {
       this.$router.replace('/404');
     }
   }
+
   update(name: string) {
     if (this.tag) {
       tagListModel.update(this.tag.id, name);
     }
   }
+
   remove() {
     if (this.tag) {
-      tagListModel.remove(this.tag.id);
+      if (tagListModel.remove(this.tag.id)) {
+        this.$router.back();
+      }else{
+        window.alert('删除失败');
+      }
     }
   }
+
   goBack() {
-    console.log('back');
     this.$router.back();
   }
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"></NumberPad>
     <Types :value.sync="record.type"></Types>
     <div class="notes">
