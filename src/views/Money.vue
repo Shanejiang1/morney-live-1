bc120021d37370f6a129c5800e08ccb84bf9fbca
Shanejiang1 <jiangshanghang@gmail.com>
2020-11-06 19:39:27 +0800

    模块化的 store 删除了 models

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,7 +3,8 @@
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"></NumberPad>
     <Types :value.sync="record.type"></Types>
     <div class="notes">
-      <FormItem field-name="备注" placeholder="在这里输入备注" @undate:value="onUpdateNotes"></FormItem>
+      <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes">
+      </FormItem>
     </div>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"></Tags>
   </Layout>
@@ -16,11 +17,12 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
+import store from '@/store/index2.ts';
 
 @Component({components: {Tags, FormItem, Types, NumberPad},})
 export default class Money extends Vue {
-  tags = window.tagList;
-  recordList = window.recordList;
+  tags = store.tagList;
+  recordList = store.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -38,7 +40,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    window.createRecord(this.record);
+    store.createRecord(this.record);
   }
 }
 </script>
